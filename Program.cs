567ca6a47ad5dfa3caa9a0a73356b80ddcb1e417
Maxcizo <137362409+Maxcizo@users.noreply.github.com>
2023-10-24 23:13:28 -0600

    Update Program.cs
    I add the query from Nalgon

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿Write("");
+using WorkingWithBD;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking; // CollectionEntry
+
+using (Almacen db = new())
+{
+
+    var Diego = db.Estudiantes
+    .Include(p => p.Grupo).Where(i => i.EstudianteId == 20300697).Include(p => p.Semestre).Where(i => i.EstudianteId == 20300697);
+
+    foreach(Estudiante e in Diego)
+    {
+
+        WriteLine($"El Id {e.EstudianteId} es de {e.Nombre} en el semestre {e.Semestre.Numero} en el grupo {e.Grupo.Nombre}");
+
+    }
+    
+}
